872a6a46929f85970bf9314925bf2db1e849f08e
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2026-04-29 19:50:13 -0400

    feat: remove accented characters

--- gpq.py
+++ gpq.py
@@ -60,8 +60,7 @@ def readImg(
     match textType:
         case ComparisonTextType.ALUM:
             cfg = (
-                "-c tessedit_char_whitelist=0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZàáâãäåéêëìíîïóôõöòøùúûüýÿ"
-                + "àáâãäåéêëìíîïóôõöòøùúûüýÿ".upper()
+                "-c tessedit_char_whitelist=0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
             )
             startRange = 3
             endRange = 7
